47205c7831aeb14de09bcc9b352b1b16e96f9270
pierre-lhoste <vingt-douze@protonmail.com>
2023-10-04 13:32:36 +0200

    Update list

--- nodown.js
+++ nodown.js
@@ -1,8 +1,6 @@
 function update() {
   const data = document.getElementById("nodown-input").value;
   const resultText = noDown(data);
-  // console.log(resultText);
-
   const render = document.getElementById("nodown-render");
   render.innerHTML = resultText;
 }
@@ -14,14 +12,42 @@ function noDown(text) {
     children: [],
   };
 
-  const createText = (text) => {
-    return [
-      {
-        type: "text",
-        children: text,
-      },
-    ];
-  };
+  // const italicRegex = /^\*{1}(.+)\*{1}/;
+  // const createItalic = (line) => {
+  //   const match = line.match(italicRegex);
+  //   const content = match[1];
+  //   const italic = {
+  //     type: "italic",
+  //     children: content,
+  //   };
+  //   return italic;
+  // };
+
+  // const boldRegex = /^\*{2}(.*)\*{2}/;
+  // const createBold = (line) => {
+  //   const match = line.match(boldRegex);
+  //   const content = match[1];
+  //   const bold = {
+  //     type: "bold",
+  //     children: content,
+  //   };
+  //   return bold;
+  // };
+
+  // const createText = (text) => {
+  //   const words = text.split(" ");
+
+  //   for (let i = 0; i < words.length; i++) {
+  //     const word = words[i];
+  //   }
+
+  //   return [
+  //     {
+  //       type: "text",
+  //       children: text,
+  //     },
+  //   ];
+  // };
 
   const titleRegex = /^(#{1,6})\s(.+)/;
   const createTitle = (line) => {
@@ -31,7 +57,7 @@ function noDown(text) {
     const title = {
       type: "title",
       level: level,
-      children: createText(content),
+      children: [content],
     };
     syntaxTree.children.push(title);
   };
@@ -51,7 +77,6 @@ function noDown(text) {
         language: blockCodeLanguage,
         children: blockCodeContent,
       };
-      console.log(blockCode);
       syntaxTree.children.push(blockCode);
       blockCodeContent = [];
       blockCodeLanguage = "";
@@ -60,89 +85,71 @@ function noDown(text) {
       isBlockCode = true;
     }
   };
-  
   const addBlockCodeContent = (line) => {
     blockCodeContent.push(line);
   };
 
   const listRegex = /^(\s*)(-|\d+\.) (.+)/;
-  let lastListLevel = -1;
-  let activeList = {};
   let listRoot = {
     type: "idk",
     children: [],
     level: 0,
   };
-  const createList = (line, afterLine) => {
-    // Récupération des donnée de l'élément de la liste
+  let stack = [];
+  function createList(line, afterLine) {
     const match = line.match(listRegex);
     const listType = match[2] === "-" ? "unordered" : "ordered";
-    const listContent = match[3];
+    const content = match[3];
 
-    // Mis a jour de son niveau
-    let listLevel = match[1].length;
-    if (listLevel % 2 == 1) listLevel--; // Changement de niveau tout les deux espaces uniquement
-    listLevel = listLevel / 2;
+    // Mis a jour du niveau
+    let level = match[1].length;
+    if (level % 2 == 1) level--; // Changement de niveau tout les deux espaces uniquement
+    level = level / 2;
+
+    // Initialisation & mise a jour de la première liste
+    if (stack.length === 0) {
+      listRoot.type = listType + "-list";
+      stack.push(listRoot);
+    }
+
+    // Mis a jour de la pile
+    while (stack.length > level + 1) {
+      stack.pop();
+    }
+
+    // Si nouvelle liste
+    if (stack.length < level + 1) {
+      const list = {
+        type: listType + "-list",
+        children: [],
+      };
+      const parentList = stack[stack.length - 1];
+      const parentElement = parentList.children[parentList.children.length - 1];
+      parentElement.children.push(list);
+      stack.push(list);
+    }
 
     // Création de l'élément
-    const listElement = {
+    let listItem = {
       type: "list-element",
-      level: listLevel,
-      children: [listContent],
+      children: [{ type: "text", children: content }],
     };
 
-    // Initialisation de la liste
-    if (lastListLevel === -1) {
-      activeList = listRoot;
-      activeList.type = listType + "-list";
-      activeList.children.push(listElement);
-    }
-
-    // Si nouvelle sous liste
-    else if (lastListLevel < listLevel) {
-      console.log("New list");
-      const newList = {
-        type: listType + "-list",
-        level: listLevel + 1,
-        children: [listElement],
-      };
-      activeList.children[activeList.children.length - 1].children.push(
-        newList
-      );
-      activeList = newList;
-    }
-
-    // Si ancienne liste
-    else if (lastListLevel > listLevel) {
-      console.log("Old list");
-      activeList = listRoot;
-      for (index = 0; index < listLevel + 1; index++) {
-        activeList = activeList.children[activeList.children.length - 1];
-      }
-      console.log({ ...activeList });
-      activeList.children.push(listElement);
-    }
-
-    // Si juste nouveau élément
-    else if (lastListLevel === listLevel) {
-      console.log("Just new item");
-      activeList.children.push(listElement);
-    }
-
-    lastListLevel = listLevel;
+    // Ajout de l'élément
+    const parent = stack[stack.length - 1];
+    parent.children.push(listItem);
 
     // Si dernier élément
     if (!listRegex.test(afterLine)) {
       syntaxTree.children.push({ ...listRoot });
-      lastListLevel = -1;
+      stack = [];
       listRoot = {
         type: "idk",
         children: [],
         level: 0,
       };
-      activeList = {};
     }
-  };
+  }
 
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
@@ -160,6 +167,7 @@ function noDown(text) {
 
   syntaxTree.children = syntaxTree.children.filter((el) => el.children !== "");
   console.table(syntaxTree.children);
+  console.log(syntaxTree);
   return objectToHTML(syntaxTree);
 }
 
@@ -172,33 +180,27 @@ function objectToHTML(obj) {
 
   if (obj.type === "root" && obj.children) {
     html += obj.children.map((child) => objectToHTML(child)).join("");
-  } 
-  else if (obj.type === "unordered-list" && obj.children) {
+  } else if (obj.type === "unordered-list" && obj.children) {
     html += "<ul>";
     html += obj.children.map((child) => objectToHTML(child)).join("");
     html += "</ul>";
-  }
-   else if (obj.type === "ordered-list" && obj.children) {
+  } else if (obj.type === "ordered-list" && obj.children) {
     html += "<ol>";
     html += obj.children.map((child) => objectToHTML(child)).join("");
     html += "</ol>";
-  }
-   else if (obj.type === "block-code" && obj.children) {
-    html += "<pre class=\"" + obj.language + "\"><code>";
+  } else if (obj.type === "block-code" && obj.children) {
+    html += '<pre class="' + obj.language + '"><code>';
     html += obj.children.map((child) => objectToHTML(child)).join("\n");
     html += "</code></pre>";
-  }
-   else if (obj.type === "list-element" && obj.children) {
+  } else if (obj.type === "list-element" && obj.children) {
     html += "<li>";
     html += obj.children.map((child) => objectToHTML(child)).join("");
     html += "</li>";
-  }
-   else if (obj.type === "title" && obj.children) {
+  } else if (obj.type === "title" && obj.children) {
     html += "<h" + obj.level + ">";
     html += obj.children.map((child) => objectToHTML(child)).join("");
     html += "</h" + obj.level + ">";
-  }
-   else if (obj.type === "text" && obj.children) {
+  } else if (obj.type === "text" && obj.children) {
     html += obj.children;
   }
 
