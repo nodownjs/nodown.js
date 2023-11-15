91e15229279a015bea88442c1f374b3f84f4664c
Pierre LHOSTE <vingt-douze@protonmail.com>
2023-11-15 22:23:16 +0100

    Update

--- package/parser.js
+++ package/parser.js
@@ -46,6 +46,7 @@ import createList, {
 import createSection from "./elements/section.js";
 import createDiv from "./elements/div.js";
 import createSubDiv from "./elements/subDiv.js";
+import { convertToObject } from "./elements/inline.js";
 
 function transformEscapedChar(match, g1) {
   return (
@@ -151,33 +152,6 @@ export default function parser(textDocument) {
     },
   ];
 
-  function removeBackslash(text, variable) {
-    if (
-      text
-        .toLowerCase()
-        .includes(varList.map((m) => "<" + m.name.toLowerCase() + ">")) &&
-      !variable
-    ) {
-      varList.forEach((m) => {
-        const varRegExp = new RegExp("<" + m.name + ">", "gi");
-        text = text.replace(varRegExp, m.content);
-      });
-    }
-    const backSlashFixerRegExp = new RegExp(
-      escapedIdentifier[0] +
-        "(" +
-        escapedCharConfig.map((c) => c.code).join("|") +
-        ")" +
-        escapedIdentifier[1],
-      "g"
-    );
-    // return text;
-    function fixEscapedChar(match, p1) {
-      return escapedCharConfig.find((c) => c.code === p1).char;
-    }
-    return text.replace(backSlashFixerRegExp, fixEscapedChar);
-  }
-
   function removeBackslashInCode(text) {
     // return text;
     const backSlashFixerRegExp = new RegExp(
@@ -192,100 +166,6 @@ export default function parser(textDocument) {
       return "\\" + escapedCharConfig.find((c) => c.code === p1).char;
     }
     return text.replace(backSlashFixerRegExp, fixEscapedChar);
-  }
-
-  function convertToObject(text) {
-    let allMatches = inlineRegExpList.map((config) => ({ ...config }));
-
-    allMatches = allMatches
-      .map((config) => {
-        const match = [...text.matchAll(config.regexp)][0];
-        // if (match && match[1].trim() !== "") {
-        if (match) {
-          config.index = match.index;
-          config.raw = match[0];
-          config.group = [...match].filter((match, index) => index > 0);
-        }
-        return config;
-      })
-      .filter((a) => a.index >= 0)
-      .sort((a, b) => a.index - b.index);
-
-    if (allMatches.length === 0) {
-      return [
-        {
-          type: "text",
-          children: removeBackslash(text),
-        },
-      ];
-    }
-
-    const match = allMatches[0];
-
-    const textBefore = removeBackslash(text.substring(0, match.index));
-
-    const textAfter = removeBackslash(
-      text.substring(match.index + match.raw.length)
-    );
-
-    const obj = {};
-
-    const result = [
-      {
-        type: "text",
-        children: textBefore,
-      },
-      obj,
-      ...convertToObject(textAfter),
-    ];
-
-    if (match.name === "image") {
-      const [alt, width, height, render] = match.group[0]
-        ? match.group[0].split(";")
-        : "";
-      const source = match.group[1];
-      const title = match.group[2];
-      if (width) obj.width = width;
-      if (height) obj.height = height;
-      if (render) obj.render = render;
-      obj.type = "image";
-      obj.title = title;
-      obj.alt = alt;
-      obj.source = source;
-    } else if (match.name === "link") {
-      obj.type = "link";
-      obj.href = match.group[1];
-      obj.title = match.group[2];
-      obj.children = convertToObject(match.group[0].trim());
-    } else if (match.name === "color") {
-      obj.type = "color";
-      obj.color = match.group[0];
-      obj.children = convertToObject(match.group[0].trim());
-    } else if (match.name === "code") {
-      obj.type = "code";
-      if (
-        match.group[1] ===
-        escapedIdentifier[0] + "&#96;" + escapedIdentifier[1]
-      ) {
-        match.group[0] = match.group[0] + "\\";
-      }
-      obj.children = removeBackslash(
-        removeBackslashInCode(match.group[0]),
-        true
-      );
-    } else if (match.name === "code-with-var") {
-      obj.type = "code";
-      if (match.group[1] === "&#96;") match.group[0] = match.group[0] + "\\";
-      obj.children = removeBackslash(
-        removeBackslashInCode(match.group[0]),
-        false
-      );
-    } else {
-      obj.type = match.name;
-      obj.children = convertToObject(match.group[0]);
-    }
-
-    return result;
   }
 
   const createParagraph = (line) => {
@@ -455,6 +335,8 @@ export default function parser(textDocument) {
         blockCodeContent = [];
         blockCodeLanguage = "";
       } else {
+        const match = line.match(blockCodeRegExp);
+        if (match[1]) blockCodeLanguage = match[1];
         isBlockCode = true;
       }
     }
