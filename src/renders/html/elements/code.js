88aed1b6b048e5760dc10976eec07002d48d8172
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-05 00:12:49 +0100

    Refactor code.js to escape special characters in content

--- src/renders/html/elements/code.js
+++ src/renders/html/elements/code.js
@@ -1,7 +1,15 @@
 import { childrenMap } from "../render";
 
 export default function createCode(obj) {
-  const content = childrenMap(obj.children);
+  let content = childrenMap(obj.children);
+  if (!obj.formatted) {
+    content = content
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;")
+      .replace(/&/g, "&amp;");
+  }
   const code = `<code>${content}</code>`;
   return code;
 }
