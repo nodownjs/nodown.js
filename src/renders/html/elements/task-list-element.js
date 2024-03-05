e7ffabf10f6d4b62a4599e4ff6939808d3d71bc1
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-06 00:26:09 +0100

    Fix task list element checkbox spacing

--- src/renders/html/elements/task-list-element.js
+++ src/renders/html/elements/task-list-element.js
@@ -1,11 +1,11 @@
 import { childrenMap } from "../render.js";
 
 export default function createTaskListElement(obj) {
-  const type = `type="checkbox"`;
+  const type = ` type="checkbox"`;
   let checked = ``;
-  if (obj.checked) checked = `checked`;
+  if (obj.checked) checked = ` checked`;
   const content = childrenMap(obj.children).replace(/^\s+/, "");
-  const check = `<input ${type} ${checked} />`;
+  const check = `<input${type}${checked} />`;
   const li = `<li>${check}${content}</li>`;
   return li;
 }
