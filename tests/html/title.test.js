5094c3a677c9d69470a0ce7b194e460d8a964f05
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-05 13:58:43 +0100

    Refactor title tests in html/title.test.js

--- tests/html/title.test.js
+++ tests/html/title.test.js
@@ -3,8 +3,9 @@ import { parser, renderToHTML } from "../../dist/main";
 import parserOptions from "./parserOptions.json";
 
 describe("Title", () => {
+  const titleText = `Cool title`;
+
   it("Basic h1", () => {
-    const titleText = `Cool title`;
     const titleTest = `# ${titleText}`;
     const titleResult = `<h1>${titleText}</h1>`;
     const title = renderToHTML(parser(titleTest, parserOptions)[0]);
@@ -12,7 +13,6 @@ describe("Title", () => {
   });
 
   it("Basic h2", () => {
-    const titleText = `Cool title`;
     const titleTest = `## ${titleText}`;
     const titleResult = `<h2 id="title-1">${titleText}</h2>`;
     const title = renderToHTML(parser(titleTest, parserOptions)[0]);
@@ -20,15 +20,20 @@ describe("Title", () => {
   });
 
   it("Basic h3", () => {
-    const titleText = `Cool title`;
     const titleTest = `### ${titleText}`;
     const titleResult = `<h3 id="title-1">${titleText}</h3>`;
     const title = renderToHTML(parser(titleTest, parserOptions)[0]);
     expect(title).toBe(titleResult);
   });
 
+  it("Basic h4", () => {
+    const titleTest = `#### ${titleText}`;
+    const titleResult = `<h4 id="title-1">${titleText}</h4>`;
+    const title = renderToHTML(parser(titleTest, parserOptions)[0]);
+    expect(title).toBe(titleResult);
+  });
+
   it("Title with id", () => {
-    const titleText = `Cool title`;
     const titleTest = `# ${titleText} {#cool-title}`;
     const titleResult = `<h1 id="cool-title">${titleText}</h1>`;
     const title = renderToHTML(parser(titleTest, parserOptions)[0]);
