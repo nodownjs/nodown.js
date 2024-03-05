009d104ba61bd55e0cfb5d7b36656fda5c696919
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-05 14:04:57 +0100

    Refactor createTitle function to remove trailing whitespace

--- src/renders/html/elements/title.js
+++ src/renders/html/elements/title.js
@@ -1,10 +1,13 @@
 import { childrenMap } from "../render";
 
 export default function createTitle(obj) {
-  const content = childrenMap(obj.children);
+  let content = childrenMap(obj.children);
   const h = `h${obj.level}`;
   let id = "";
-  if (obj.id) id = ` id="${obj.id}"`;
+  if (obj.id) {
+    id = ` id="${obj.id}"`;
+    content = content.replace(/\s+$/, "");
+  }
   const heading = `<${h}${id}>${content}</${h}>`;
   return heading;
 }
