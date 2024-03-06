5131e75628224ee408e8f194436641016cdfb47d
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-06 23:03:32 +0100

    Refactor footnote rendering in tests

--- tests/html/footnote-section.test.js
+++ tests/html/footnote-section.test.js
@@ -2,15 +2,6 @@ import { describe, expect, it } from "vitest";
 import { parser, renderToHTML } from "../../src/index";
 import parserOptions from "../parserOptions.json";
 
-function generateTest(elementTest) {
-  return renderToHTML(
-    parser(elementTest, {
-      ...parserOptions,
-      section: { disabled: false },
-    })[1]
-  );
-}
-
 const footnoteId = `footnote-1`;
 const footnoteText = `Some footnote text`;
 const text = `Some simple text`;
@@ -18,9 +9,37 @@ const text = `Some simple text`;
 describe("Footnote", () => {
   it("Basic footnote section", () => {
     const footnoteTest = `${text}[^${footnoteId}]\n[^${footnoteId}]: ${footnoteText}`;
-    console.log("🚀 ~ footnoteTest:", footnoteTest);
+    const footnoteResult = `<section id="footnotes"><div><div><ol><li class="footnote" id="fn-${footnoteId}">${footnoteText}<a href="#fnref-${footnoteId}" > ↩</a></li></ol></div></div></section>`;
+    const footnote = renderToHTML(
+      parser(footnoteTest, {
+        ...parserOptions,
+        section: { disabled: false },
+        horizontalAlignment: { disabled: false },
+      })[1]
+    );
+    expect(footnote).toBe(footnoteResult);
+  });
+
+  it("Without div", () => {
+    const footnoteTest = `${text}[^${footnoteId}]\n[^${footnoteId}]: ${footnoteText}`;
     const footnoteResult = `<section id="footnotes"><ol><li class="footnote" id="fn-${footnoteId}">${footnoteText}<a href="#fnref-${footnoteId}" > ↩</a></li></ol></section>`;
-    const footnote = generateTest(footnoteTest);
+    const footnote = renderToHTML(
+      parser(footnoteTest, {
+        ...parserOptions,
+        section: { disabled: false },
+      })[1]
+    );
+    expect(footnote).toBe(footnoteResult);
+  });
+
+  it("Without section and div", () => {
+    const footnoteTest = `${text}[^${footnoteId}]\n[^${footnoteId}]: ${footnoteText}`;
+    const footnoteResult = `<ol><li class="footnote" id="fn-${footnoteId}">${footnoteText}<a href="#fnref-${footnoteId}" > ↩</a></li></ol>`;
+    const footnote = renderToHTML(
+      parser(footnoteTest, {
+        ...parserOptions,
+      })[1]
+    );
     expect(footnote).toBe(footnoteResult);
   });
 });
