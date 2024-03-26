e84f4653743bf24198c630c66d7b21009d683886
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-26 14:55:20 +0100

    Add new test sites

---
+++ test-site/to-html-no-div/index.js
@@ -0,0 +1,79 @@
+import { parser, renderToHTML } from "../../../dist/main.js";
+const packagePath = "../../";
+
+let theme = localStorage.getItem("theme") || "dark";
+updateTheme(theme);
+
+const linkElement = createLinkElement("base");
+linkElement.href = packagePath + "styles/index.css";
+
+function updateTheme(theme) {
+  const linkElement =
+    document.getElementById("theme") || createLinkElement("theme");
+  linkElement.href = `${packagePath}styles/theme-${theme}.css`;
+  document.body.className = `theme-${theme}`;
+  document.body.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+}
+
+function createLinkElement(id) {
+  const linkElement = document.createElement("link");
+  linkElement.id = id;
+  linkElement.rel = "stylesheet";
+  document.head.appendChild(linkElement);
+  return linkElement;
+}
+
+const updateDoc = () => {
+  const data = document.getElementById("nodown-input").value;
+
+  var startTime = performance.now();
+  const syntaxTree = parser(data, {
+    section: {
+      disabled: false,
+    },
+    horizontalAlignment: {
+      disabled: true,
+    },
+  });
+  var endTime = performance.now();
+
+  // console.table(syntaxTree.children);
+  console.log(syntaxTree);
+
+  const htmlResult = renderToHTML(syntaxTree, {
+    // title: {
+    //   raw: false,
+    // },
+    // link: {
+    //   disabled: false,
+    //   childrenFormat: "string",
+    //   customRender: (obj) => {
+    //     const strong = document.createElement("strong");
+    //     strong.innerHTML = `${obj.children} - ${obj.href}`;
+    //     return strong;
+    //   },
+    // },
+    // root: {
+    //   disabled: false,
+    //   customId: "nodown-render",
+    // },
+  });
+
+  console.log(`Took ${endTime - startTime} milliseconds`);
+
+  const render = document.getElementById("nodown-output");
+  render.innerHTML = htmlResult;
+};
+
+function switchPls() {
+  var theme = document.body.classList[0];
+  if (theme == "theme-light") {
+    updateTheme("dark");
+  } else {
+    updateTheme("light");
+  }
+}
+
+window.updateDoc = updateDoc;
+window.switchPls = switchPls;
